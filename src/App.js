2297e5a4e72c39121640b5654e2affa17ff25911
Dey11subrata <deysubrata2311@gmail.com>
2023-11-21 12:14:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { AllRoutes } from "./routes/AllRoutes";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to CineMetry</h1>
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
