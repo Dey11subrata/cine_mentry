bb59aff6e504c543a623b9c04965a627d7737f0e
Dey11subrata <deysubrata2311@gmail.com>
2023-11-23 18:24:55 +0530

    movie card created

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { MovieCard } from "../components/MovieCard";
 
 export const MovieList = () => {
-  return <main>MovieList</main>;
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly "></div>
+      </section>
+    </main>
+  );
 };
