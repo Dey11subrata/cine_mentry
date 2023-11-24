ad0efb45052ed32982e0cf557d6c9cd0f2567c2e
Dey11subrata <deysubrata2311@gmail.com>
2023-11-24 23:35:15 +0530

    search movie feature implemented

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 import Logo from "../assets/logo.png";
 
@@ -31,6 +31,16 @@ export const Header = () => {
       ? document.documentElement.classList.add("dark")
       : document.documentElement.classList.remove("dark");
   }, [darkMode]);
+
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const searchValue = event.target.search.value;
+    console.log(searchValue);
+    event.target.reset();
+    return navigate(`/search?q=${searchValue}`);
+  };
 
   return (
     <header>
@@ -136,13 +146,16 @@ export const Header = () => {
                 </svg>
                 <span className="sr-only">Search icon</span>
               </div>
-              <input
-                type="text"
-                id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
-                autoComplete="off"
-              />
+              <form onSubmit={handleSearch}>
+                <input
+                  type="text"
+                  id="search-navbar"
+                  name="search"
+                  className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search..."
+                  autoComplete="off"
+                />
+              </form>
             </div>
 
             {/* <- search bar it will be hidden in mobile view */}
@@ -201,13 +214,16 @@ export const Header = () => {
                   />
                 </svg>
               </div>
-              <input
-                type="text"
-                id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
-                autoComplete="off"
-              />
+              <form onSubmit={handleSearch}>
+                <input
+                  type="text"
+                  id="search-navbar"
+                  name="search"
+                  className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search..."
+                  autoComplete="off"
+                />
+              </form>
             </div>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,20 @@
-import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import { MovieCard } from "../components/MovieCard";
 
-export const Search = () => {
-  return <main>Search</main>;
+export const Search = ({ apiPath }) => {
+  const [searchParams] = useSearchParams();
+  const queryParam = searchParams.get("q");
+  const { data: movieList } = useFetch(apiPath, queryParam);
+  return (
+    <main>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap other:justify-evenly ">
+          {movieList.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 };
