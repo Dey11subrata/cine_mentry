0bc56852309a9aee37e8f9131e2fdf9b22059815
Dey11subrata <deysubrata2311@gmail.com>
2023-11-24 17:35:50 +0530

    pages accourding to route implemented

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
-export const useFetch = (url) => {
+export const useFetch = (apiPath) => {
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=1bc4184f5cc98dd17a601b55f914e680`;
   const [data, setData] = useState([]);
   useEffect(() => {
     async function fetchMovies() {
