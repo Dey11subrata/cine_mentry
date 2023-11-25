ef429efa97b06d2f1bf1dc7e0ec8f2fbdd4822b8
Dey11subrata <deysubrata2311@gmail.com>
2023-11-25 23:19:59 +0530

    temp

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 
 export const useFetch = (apiPath, queryParam = "") => {
-  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryParam}`;
+  // const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryParam}`;
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=1bc4184f5cc98dd17a601b55f914e680&query=${queryParam}`;
   const [data, setData] = useState([]);
   useEffect(() => {
     async function fetchMovies() {
