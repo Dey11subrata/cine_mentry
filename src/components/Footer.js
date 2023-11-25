fb3a91c07139b9d1433dcd2ed272bef3a41970de
Dey11subrata <deysubrata2311@gmail.com>
2023-11-26 00:45:59 +0530

    rel attribute in footer links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export const Footer = () => {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <Link href="/" className="hover:underline">
+          <Link to="/" className="hover:underline">
             CineMentry™
           </Link>
           . All Rights Reserved.
@@ -17,6 +17,7 @@ export const Footer = () => {
             <a
               href="https://github.com/Dey11subrata"
               target="_blank"
+              rel="noreferrer"
               className="hover:underline me-4 md:me-6"
             >
               GitHub
@@ -26,6 +27,7 @@ export const Footer = () => {
             <a
               href="https://www.linkedin.com/in/subrata-dey-4653881a4/"
               target="_blank"
+              rel="noreferrer"
               className="hover:underline me-4 md:me-6"
             >
               LinkedIn
@@ -35,13 +37,14 @@ export const Footer = () => {
             <a
               href="mailto:deysubrata2311@gmail.com"
               target="_blank"
+              rel="noreferrer"
               className="hover:underline me-4 md:me-6"
             >
               Email
             </a>
           </li>
           <li>
-            <a href="#" className="hover:underline">
+            <a href="/" className="hover:underline">
               Contact
             </a>
           </li>
